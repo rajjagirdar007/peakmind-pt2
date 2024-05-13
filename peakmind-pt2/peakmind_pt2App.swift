757ecc253e319113a2cdd8bd667400b26579218a
Raj Jagirdar <jagirdar.r@northeastern.edu>
2024-05-13 17:38:50 -0400

    Initial Commit

---
+++ peakmind-pt2/peakmind_pt2App.swift
@@ -0,0 +1,17 @@
+//
+//  peakmind_pt2App.swift
+//  peakmind-pt2
+//
+//  Created by Raj Jagirdar on 5/13/24.
+//
+
+import SwiftUI
+
+@main
+struct peakmind_pt2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
